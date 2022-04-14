77b817156d5c35d491c38cdf82d481cbed01ba46
luke-song <hyeonminsong7@gmail.com>
2022-04-14 16:43:05 -0700

    Video Background <Masthead />

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Masthead from '../components/masthead'
 
 const Home: NextPage = () => {
   return (
@@ -11,20 +12,8 @@ const Home: NextPage = () => {
         <meta name="description" content="Prototype of Decentra Sammy" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className="font-bold text-9xl">Hello</div>
       
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
+      <Masthead/>
     </div>
   )
 }
