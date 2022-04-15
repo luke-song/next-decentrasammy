f8db7968cddbb919543872b68eec49d691b068f2
luke-song <hyeonminsong7@gmail.com>
2022-04-15 07:08:44 -0700

    Aboutus/Member Grid Layout, team images imported

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Masthead from '../components/masthead'
+import Aboutus from '../components/aboutus'
 
 const Home: NextPage = () => {
   return (
@@ -12,8 +13,8 @@ const Home: NextPage = () => {
         <meta name="description" content="Prototype of Decentra Sammy" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      
       <Masthead/>
+      <Aboutus/>
     </div>
   )
 }
