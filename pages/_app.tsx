4884c28099f3bf2a9b90b3da6e31c1ef4539e8f8
luke-song <hyeonminsong7@gmail.com>
2022-04-17 05:45:06 -0700

    all the type error fixed

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,8 @@ import type { AppProps } from 'next/app'
 import ScrollObserver from '../utils/scroll-observer'
 
 function MyApp({ Component, pageProps }: AppProps) {
+
+  
   return (
   <ScrollObserver>
   <Component {...pageProps} />
--- utils/scroll-observer.tsx
+++ utils/scroll-observer.tsx
@@ -8,7 +8,11 @@ export const ScrollContext = React.createContext<ScrollValue>({
     scrollY:0
 })
 
-const ScrollObserver: React.FC = ({children}) => {
+interface Observer {
+    children: any
+}
+
+const ScrollObserver: React.FC<Observer> = ({children}) => {
     const [scrollY, setScrollY] = useState(0)
     const handleScroll = useCallback(() => {
         setScrollY(window.scrollY)
